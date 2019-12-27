2888e86e70f655cb26edca7eb48890dab642d8a0
krasnovaol <mr.february@bk.ru>
2019-12-27 11:24:28 +0300

    ...

--- client.py
+++ client.py
@@ -1,5 +1,21 @@
 import socket
 import pickle
+from random import randint
+
+def combinaton(P1, key):
+    P2 = []
+    for i in range(len(P1)):
+        P2 += chr(ord(P1[i]) ^ key)
+    return ''.join(P2)
+
+def send_message(sock, message, key):
+    message = combinaton(message, key)
+    sock.send(pickle.dumps(message))
+
+def receive(sock, key):
+    message = pickle.loads(sock.recv(1024))
+    message = combinaton(message, key)
+    return message
 
 HOST = '127.0.0.1'
 PORT = 8080
@@ -7,8 +23,19 @@ PORT = 8080
 sock = socket.socket()
 sock.connect((HOST, PORT))
 
-p, g, a = 7, 5, 3
+p = randint(0,10000)
+g = randint(0,10000)
+a = randint(0,10000)
+
 A = g ** a % p
 sock.send(pickle.dumps((p, g, A)))
+
+B = pickle.loads(sock.recv(1024))
+key = B ** a % p
+message = input("Сообщение: ")
+while message != 'exit':
+    send_message(sock, message, key)
+    print(receive(sock, key))
+    message = input()
 
 sock.close()
--- server.py
+++ server.py
@@ -1,5 +1,21 @@
 import socket
 import pickle
+from random import randint
+
+def combinaton(P1, key):
+    P2 = []
+    for i in range(len(P1)):
+        P2 += chr(ord(P1[i]) ^ key)
+    return ''.join(P2)
+
+def send_message(sock, message, key):
+    message = combinaton(message, key)
+    sock.send(pickle.dumps(message))
+
+def receive(sock, key):
+    message = pickle.loads(sock.recv(1024))
+    message = combinaton(message,key)
+    return message
 
 HOST = '127.0.0.1'
 PORT = 8080
@@ -9,7 +25,21 @@ sock.bind((HOST, PORT))
 sock.listen(1)
 conn, addr = sock.accept()
 
-msg = conn.recv(1024)
-print(pickle.loads(msg))
+message = conn.recv(1024)
+
+p, g, A = pickle.loads(message)
+b = randint(0, 10000)
+B = g ** b % p
+conn.send(pickle.dumps(B))
+key = A ** b % p
+
+
+while True:
+    try:
+        message = receive(conn, key)
+        print(message)
+        send_message(conn, 'Сообщение успешно получено и расшифровано', key)
+    except EOFError: 
+        break
 
 conn.close()
